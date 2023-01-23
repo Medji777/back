39621800ed7e7e178b1470a7e53642f7e04a6273
Vladimir Dergachev <>
2023-01-23 11:21:28 +0300

    fix name db

--- src/repositories/db.ts
+++ src/repositories/db.ts
@@ -10,7 +10,7 @@ const URI = process.env.mongoURI || "mongodb://0.0.0.0:27017";
 
 export const client = new MongoClient(URI);
 
-const db = client.db("back");
+const db = client.db();
 export const blogsCollection = db.collection<BlogsViewModel>('blogs');
 export const postsCollection = db.collection<PostsViewModel>('posts');
 export const videosCollection = db.collection<IVideo>('videos');
@@ -18,7 +18,7 @@ export const videosCollection = db.collection<IVideo>('videos');
 export async function runDb () {
     try {
         await client.connect();
-        await client.db("back").command({ping: 1})
+        await client.db().command({ping: 1})
         console.log("Connected to DB Ok!",URI)
     }
     catch (e) {
