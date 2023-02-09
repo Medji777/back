28bef3f609332248a0bc0a0c7d6187c2cf6c5e6c
Vladimir Dergachev <>
2023-02-10 01:03:17 +0300

    fix

--- src/routers/comments.ts
+++ src/routers/comments.ts
@@ -1,9 +1,11 @@
 import {Router} from "express";
 import {deleteComments, getComments, updateComments} from "../controllers/comments.controller";
-import {bearerAuthMiddleware} from "../middlewares";
+import {bearerAuthMiddleware, validateMiddleware} from "../middlewares";
+import {validatorBodyContent} from "../validations";
 
 export const commentsRouter = Router({});
+const validateBody = validateMiddleware([validatorBodyContent])
 
 commentsRouter.get('/:id',getComments)
-commentsRouter.put('/:id',bearerAuthMiddleware,updateComments)
+commentsRouter.put('/:id',bearerAuthMiddleware,validateBody,updateComments)
 commentsRouter.delete('/:id',bearerAuthMiddleware,deleteComments)
