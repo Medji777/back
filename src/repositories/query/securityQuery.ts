030e6c85b128e92f44f23a69fbdd9c8a149db5cb
Vladimir Dergachev <>
2023-03-12 12:26:05 +0300

    fix

--- src/repositories/query/securityQuery.ts
+++ src/repositories/query/securityQuery.ts
@@ -1,10 +1,10 @@
 import {sessionsCollection} from "../db";
-import {DeviceModel} from "../../types/security";
+import {DeviceModel,DeviceViewModel} from "../../types/security";
 
 export const securityQueryRepository = {
-    async getAllActiveSessions(userId: string): Promise<Array<DeviceModel>>{
+    async getAllActiveSessions(userId: string): Promise<Array<DeviceViewModel>>{
         return sessionsCollection
-            .find({userId},{projection: {_id:0}})
+            .find({userId},{projection: {_id:0, expiredTokenDate: 0}})
             .toArray()
     },
     async findSession(userId: string, deviceId: string): Promise<DeviceModel | null>{
